38f1a9c35f1550dc2edd9dd70bceba12ec425306
Valentijn Kilian <valentijnkilian@gmail.com>
2023-11-17 14:05:12 +0100

    merge

--- main.py
+++ main.py
@@ -1,16 +1,44 @@
+import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
-import matplotlib.pyplot as plt
+
 # import datasets
 df = pd.read_csv('train.csv')
+pd.set_option('display.max_columns', None)
 
-print(df.head())
-print(df.info())
+# Data verkenning
+# print(df.describe())
 
-year = df['YR']
-sns.regplot(data=df, y='LotArea', x='YrSold')
-plt.show()
+# Correlatie Matrix
+# corr_matrix = df.corr()
+# print(corr_matrix)
+# sns.heatmap(corr_matrix, annot=False, cmap='coolwarm', fmt='.2f')
+# plt.show()
 
 
+# sns.histplot(df['SalePrice'], bins=30, kde=True)
+# plt.show()
 
+# Missende gegevens
+# Controleer voor missende gegevens
+missing_data = df.isnull().sum()
+# print(missing_data[missing_data > 0])
 
+# Data aanvullen
+df['Alley'] = df['Alley'].fillna(0)
+alley = {'Grvl': 2, 'Pave': 1, 0: 0}
+df['Alley'] = df['Alley'].map(alley)
+
+df['LotFrontage'] = df['LotFrontage'].fillna(0)
+
+# Vult NaN waardes met 0
+df['MasVnrType'] = df['MasVnrType'].fillna(0)
+# Dit is een dictionary met de text waardes (Zie data description) en hun numerieke waardes
+brick_type = {'BrkCmn': 5, 'BrkFace': 4, 'CBlock': 3, 'None': 2, 'Stone': 1, 0: 0}
+# Dit mapt automatisch de numerieke waardes aan de tekst waardes
+df['MasVnrType'] = df['MasVnrType'].map(brick_type)
+# Controle of de map goed is uitgevoerd. Mag geen NaN meer bevatten!
+print(df['MasVnrType'].unique())
+# Deze code controleert of alle lege waardes zijn vervangen met numerieke waardes.
+missing_data = df.isnull().sum()
+print(missing_data[missing_data > 0])git add 
